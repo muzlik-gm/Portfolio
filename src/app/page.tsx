79cf7f58675045705336361d5c1bfcbc7f8c73c6
Hamza Khan <muzlikgamer@gmail.com>
2025-11-02 10:40:08 -0800

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,15 @@ import { HeroSection, ProjectsSection, AboutSection, ContactSection } from "@/co
 export default function Home() {
   return (
     <Layout>
-      {/* First impressions matter - let's make it count! */}
       <HeroSection />
-
-      {/* A little visual break to keep things interesting */}
       <SectionDivider type="layered-wave" />
-
-      {/* Here's my story - who I am and what drives me */}
+      
       <AboutSection />
-
-      {/* Another smooth transition */}
       <SectionDivider type="wave" flip />
-
-      {/* The fun part - showing off what I've built! */}
+      
       <ProjectsSection />
-
-      {/* One more elegant divider */}
       <SectionDivider type="layered-wave" />
-
-      {/* Let's connect! I'd love to hear from you */}
+      
       <ContactSection />
     </Layout>
   );
